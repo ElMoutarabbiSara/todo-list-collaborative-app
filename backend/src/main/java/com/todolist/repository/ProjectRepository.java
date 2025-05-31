b6dfd909060635fa28dcf654a088396abaa6a395
ElMoutarabbiSara <165937897+ElMoutarabbiSara@users.noreply.github.com>
2025-05-31 20:49:40 +0100

    Delete backend/src/main/java/com/todolist/repository directory

--- backend/src/main/java/com/todolist/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.todolist.repository;
-
-import com.todolist.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
